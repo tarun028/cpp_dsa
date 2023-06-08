223d55fdd2f4cd5ff977206c5089f1a51035efb7
tarun028 <tarunsrivastavakvs@gmail.com>
2023-06-08 23:56:12 +0530

    stl

--- STL/one.cpp
+++ STL/one.cpp
@@ -1,8 +1,12 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
 
 
-
 int main(){
+    
+    vector<int> v;
+    v.push_back(1);
+    cout<<"capacity->"<<v.capacity()<<endl;
     return 0;
 }
