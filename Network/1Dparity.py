58e62d9cb5eccfd66c30ef9f22fb54cd1c091fdb
tarun028 <tarunsrivastavakvs@gmail.com>
2023-07-20 04:02:45 +0530

    jojo

--- Network/1Dparity.py
+++ Network/1Dparity.py
@@ -1,19 +1,34 @@
-def calculate_checksum_1d(data):
-    checksum = sum(data)
-    return checksum
+def crc_division(message, generator_polynomial):
+    remainder = message[:]
+    generator_len = len(generator_polynomial)
 
-def main_1d():
-    # Read 1D array data from the user
-    data = list(map(int, input("Enter 1D array data (space-separated integers): ").split()))
+    # Pad the message with zeros
+    remainder.extend([0] * (generator_len - 1))
 
-    # Calculate the checksum
-    checksum = calculate_checksum_1d(data)
+    # Perform the CRC division process
+    for i in range(len(remainder) - generator_len + 1):
+        if remainder[i] == 1:
+            for j in range(generator_len):
+                remainder[i + j] ^= generator_polynomial[j]
 
-    # Append the checksum to the end of the array
-    data.append(checksum)
+    # Return the final value of the remainder (CRC value)
+    return remainder[-(generator_len - 1):]
 
-    # Display the array with the checksum
-    print("Data with checksum:", data)
+def main():
+    # Read message from the user
+    message = list(map(int, input("Enter the message (binary): ")))
+
+    # Read generator polynomial from the user
+    generator_polynomial = list(map(int, input("Enter the generator polynomial (binary): ")))
+
+    # Calculate CRC value
+    crc_value = crc_division(message, generator_polynomial)
+
+    # Append the CRC value to the original message
+    transmitted_message = message + crc_value
+
+    # Display the transmitted message with the CRC value
+    print("Transmitted Message with CRC:", transmitted_message)
 
 if __name__ == "__main__":
-    main_1d()
+    main()
